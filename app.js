f1040df3f549068937f60c76558131383fff874d
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-10-09 08:06:02 +0300

    login and register front end functionality

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
-const cors = require("cors")
-app.use(cors())
 const port = 5000;
-const cors = require('cors');
 
 app.use(cors());
-
-//database connection
-const connection = require('./database/db.config');
-
-// Questions middleware router file
-const questionsRoutes = require("./routes/question.routes");
-const answerRoutes = require("./routes/answer.routes");
-const auth = require("./middleware/auth.middleware");
-
-//questions middleware router file
-const questionsRoutes = require('./routes/question.routes');
-
-// answer routes middleware file
-const answerRoute = require('./routes/answers.routes');
-
-//authentication middleware router file
-const auth = require('./middleware/auth.middleware');
-
-//json middleware
 app.use(express.json());
 
-// Users middleware
+const connection = require('./database/db.config');
+
+const usersRoutes = require("./routes/users.routes");
+const questionsRoutes = require("./routes/question.routes");
+const answerRoutes = require("./routes/answers.routes");
+const auth = require("./middleware/auth.middleware");
+
 app.use("/api/users", usersRoutes);
-
-// Questions
 app.use("/api/questions", auth, questionsRoutes);
-
-// Answers
-app.use("/api/answers", auth, answerRoutes); 
-
-//answer middleware
-app.use('/api/answers', auth, answerRoute);
+app.use("/api/answers", auth, answerRoutes);
 
 (async () => {
   try {
     const result = await connection.execute("SELECT 'test'");
+    console.log(result); // Print the result of the test query
 
     await app.listen(port);
-    console.log("Database connection established:)");
+    console.log("Database connection established :)");
     console.log(`Listening on ${port}: http://localhost:${port}`);
   } catch (err) {
     console.error(err.message);
--- database/db.config.js
+++ database/db.config.js
@@ -1,23 +1,17 @@
-const mysql2 = require('mysql2');
-const dotenv = require('dotenv');
+const mysql2 = require("mysql2");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-//access the database
 const connection = mysql2.createPool({
-  host: 'localhost',
-<<<<<<< HEAD
-  user: process.env.USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-=======
+  host: "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
->>>>>>> f6f6e1046f4c7df78854c99dd4531451d00301b2
-  connectionLimit: 10, // Maximum number of connections allowed
+  connectionLimit: 10,
 });
 
+module.exports = connection.promise();
 
 //connection test
 // connection.execute(`select 'test'`, (err, result) => {
