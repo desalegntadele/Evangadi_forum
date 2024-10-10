ce7e4c6edd8cc880db28b25f8c95f5c550046eec
mintesnot <mintesnottesfaye@gmail.com>
2024-10-10 13:21:01 +0300

    heder css and component chaged using react bootstrap

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./header.css";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/evangadi-logo-black.png";
 import { AppState } from "../../App";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 
 const Header = () => {
   const { user, setUser } = useContext(AppState); // Make sure you have setUser available
@@ -13,46 +14,46 @@ const Header = () => {
     setIsLoggedIn(!!user); // Set true or false based on user existence
   }, [user]);
 
-    const handleButtonClick = () => {
-      if (isLoggedIn) {
-        // If the user is logged in, handle logout
-        setUser(null); // Clear user context or state
-        navigate("/"); // Navigate to home page after logout
-      } else {
-        // If the user is not logged in, navigate to the register page
-        navigate("/register");
-      }
-    };
-
-//   const handleButtonClick = () => {
-//     setIsLoggedIn((prevIsLoggedIn) => !isLoggedIn);
-//   };
+  const handleButtonClick = () => {
+    if (isLoggedIn) {
+      // If the user is logged in, handle logout
+      setUser(null); // Clear user context or state
+      navigate("/"); // Navigate to home page after logout
+    } else {
+      // If the user is not logged in, navigate to the register page
+      navigate("/register");
+    }
+  };
 
   return (
-    <section className="Header">
-      <div className="Header_container">
-        <div className="logo_container">
-          <Link to="/">
-            <img src={logo} alt="logo" />
-          </Link>
-        </div>
-        <div className="Header_linkBtn">
-          <div className="Header_container_link">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/">How it Works</Link>
-              </li>
-            </ul>
-          </div>
-          <button onClick={handleButtonClick}>
-            {isLoggedIn ? "Logout" : "Sign In"}
-          </button>
-        </div>
-      </div>
-    </section>
+    <Navbar
+      bg="white"
+      expand="lg"
+      sticky="top"
+      className="shadow-sm border-bottom">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="logo" style={{ width: "170px" }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              How it Works
+            </Nav.Link>
+            <Button
+              className="Btn"
+              variant="primary"
+              onClick={handleButtonClick}>
+              {isLoggedIn ? "Logout" : "Sign In"}
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
